a6a2b909ba4558e26527fc2b626ccc3c8b257e47
Jordan <39-12602@sae.edu.za>
2025-05-06 13:15:14 +0200

    extract features from audio

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tempfile
 import os
 import essentia
 import essentia.standard as es
+import numpy as np
 
 app = FastAPI()
 
@@ -47,26 +48,102 @@ async def get_songs():
             audio = loader() #actual audio
             print(f"[{idx}] Audio loaded. {len(audio)} samples")
 
-            #processing will happen here
+            #frame generator
+            frame_generator = es.FrameGenerator(audio, frameSize=2048, hopSize=1024, startFromZero=True)
+            windowing = es.Windowing(type='hann')
+            spectrum = es.Spectrum(size=2048) #create spectrum to be analyzed
+            centroid = es.Centroid() #create centroid
+
+            #MFCC
+            mfcc = es.MFCC(highFrequencyBound=18000, inputSize=1025)
+            melbands = es.MelBands(numberBands=40)
+
+            #Flux
+            flux = es.Flux()
+            prev_spectrum = None
+
+
+            flux_values = []
+            centroids = []
+            mfccs = []
+
+
+
+            for frame in frame_generator:
+                win = windowing(frame)
+                spec = spectrum(win)
+                #append centroids
+                c = centroid(spec)
+                centroids.append(c)
+                #append mfccs
+                mfcc_bands, mfcc_coeffs = mfcc(spec)
+                mfccs.append(mfcc_coeffs)
+
+                #flux
+                if prev_spectrum is not None:
+                    flux_val = flux(spec - prev_spectrum)
+                    flux_values.append(flux_val)
+                prev_spectrum = spec
+
+
+
+
+            #BPM
             rhythm_extractor = es.RhythmExtractor2013()
             rhythm_features = rhythm_extractor(audio)
-            bpm = rhythm_features[0]   
+            bpm = rhythm_features[0]   #gets average over whole track
 
+            #Danceability
             danceability_extractor = es.Danceability()
             all_danceability = danceability_extractor(audio) 
-            danceability = all_danceability[0]
+            danceability = all_danceability[0] #gets average over whole track
 
+            #Energy
+            energy_extractor = es.Energy()
+            energy = energy_extractor(audio)
+
+            #Key
+            key_extractor = es.KeyExtractor(profileType="edma") #uses profile adapted to Electronic dance music
+            key, scale, strength = key_extractor(audio)
+
+            #Loudness
+            loudness = es.Loudness()(audio)
+
+            #spectral centroid
+            spectral_centroid = np.mean(centroids) # value 0-1 -> lower number = lower frequnecies
+
+            #mfccs
+            mfccs_np = np.array(mfccs)
+            mean_mfcc = np.mean(mfccs_np, axis=0).tolist() #coefinents to represent tonal frequencies- tone/timbre
+
+            #flux
+            avg_flux = np.mean(flux_values) if flux_values else 0.0
+
+
+            print(f"Flux: {avg_flux}")
+            print(f"MFCC: {mean_mfcc}")
+            print(f"Centroid: {spectral_centroid}")
+            print(f"Loudness: {loudness}")
+            print(f"Key: {key} {scale} (strength: {strength:.2f})")
+            print(f"Energy: {energy}")
             print(f"Danceability: {danceability}")
             print(f"BPM: {bpm}")
 
 
             #create new object
             analysis_results.append({
-                "count": idx,
+                "Track": idx,
                 "title": song["title"],
                 "url": song["url"],
                 "danceability": round(danceability, 2),
-                "BPM": round(bpm, 2)
+                "BPM": round(bpm, 2),
+                "energy": round(energy, 2),
+                "key": f"{key} {scale} {round(strength,2)}",
+                "loudess": f"{round(loudness,2)}",
+                "spectral-centroid": f"{round(spectral_centroid,2)}",
+                "mfcc": [round(coeff, 3) for coeff in mean_mfcc],
+                "flux": round(avg_flux, 4)
+                
             })
 
         finally:#remove temp file
